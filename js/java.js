2c935e20269b39cf492985f1b63ebeb904e8a580
Angelikandersson <angelika.andersson@elev.ga.ntig.se>
2023-03-22 20:05:38 +0100

    ok

---
+++ js/java.js
@@ -0,0 +1,8 @@
+let button = document.querySelector(".icon").addEventListener("click", showmenu);
+let bev = document.querySelector("#hej");
+
+
+function showmenu(){
+    console.log("huh!")
+    bev.classList.toggle("hide");
+    }
